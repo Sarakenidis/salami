e90eec6dbb42d5f990d7dc51ad5a5873ab7822b9
Kwnstantinos <33523826+Sarakenidis@users.noreply.github.com>
2017-11-12 23:59:54 +0200

    Commit Staged

--- salami/Form2.Designer.cs
+++ salami/Form2.Designer.cs
@@ -19,7 +19,7 @@
             }
             base.Dispose(disposing);
         }
-        sddsds
+        //gewgewgewweewgwe
         #region Windows Form Designer generated code
 
         /// <summary>
